2ec3a034589d1ff1a474c70f287d3e378c5969e4
lbkhoang <khanhhoangleba@gmail.com>
2020-04-30 09:16:16 +0700

    1st commit

---
+++ app/src/main/java/com/example/ecommerceapplication/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.ecommerceapplication;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button signUpButton, loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        signUpButton = (Button) findViewById(R.id.sign_up_btn);
+        loginButton = (Button) findViewById(R.id.login_btn);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}

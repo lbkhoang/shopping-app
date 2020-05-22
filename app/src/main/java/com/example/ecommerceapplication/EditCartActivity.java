0decb2feb7ae5878974a043b8ae97e6e45bb0ea4
lbkhoang <khanhhoangleba@gmail.com>
2020-05-22 09:00:40 +0700

    updata total price

--- app/src/main/java/com/example/ecommerceapplication/EditCartActivity.java
+++ app/src/main/java/com/example/ecommerceapplication/EditCartActivity.java
@@ -4,36 +4,40 @@ import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import com.example.ecommerceapplication.Model.Orders;
 import com.example.ecommerceapplication.Model.Products;
 import com.example.ecommerceapplication.Model.Users;
 import com.example.ecommerceapplication.ViewHolder.OrderViewHolder;
-import com.example.ecommerceapplication.ViewHolder.ProductViewHolder;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.*;
 import com.squareup.picasso.Picasso;
 import io.paperdb.Paper;
 
-import static android.util.Log.d;
 
 public class EditCartActivity extends AppCompatActivity {
 
     private DatabaseReference OrderRef;
     private RecyclerView recyclerView;
     private RecyclerView.LayoutManager layoutManager;
+    private Button checkOutButton;
+    private TextView txtTotalPrice;
     private Users user;
+    private int totalPrice = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_cart);
+
+        checkOutButton = findViewById(R.id.check_out_btn);
+        txtTotalPrice = findViewById(R.id.txtTotal);
 
         Paper.init(this);
         user = Paper.book().read("userDetail");
@@ -44,17 +48,24 @@ public class EditCartActivity extends AppCompatActivity {
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
 
+
+        checkOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkout();
+            }
+        });
+        setData();
+        checkout();
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
+
+    protected void setData() {
 
         FirebaseRecyclerOptions<Products> options =
                 new FirebaseRecyclerOptions.Builder<Products>()
                         .setQuery(OrderRef, Products.class)
                         .build();
-
 
         FirebaseRecyclerAdapter<Products, OrderViewHolder> adapter =
                 new FirebaseRecyclerAdapter<Products, OrderViewHolder>(options) {
@@ -71,6 +82,10 @@ public class EditCartActivity extends AppCompatActivity {
                         holder.txtProductName.setText(model.getPname());
                         holder.txtProductPrice.setText("Price = " + model.getPrice() + "$");
                         holder.txtProductQuantity.setText("Amount = " +model.getQuantity());
+
+                        int productTotalCost = Integer.parseInt(model.getPrice()) * Integer.parseInt(model.getQuantity());
+                        totalPrice = totalPrice + productTotalCost;
+
                         Picasso.get().load(model.getImage()).into(holder.imageView);
                         holder.imageView.setOnClickListener(new View.OnClickListener() {
                             @Override
@@ -86,5 +101,11 @@ public class EditCartActivity extends AppCompatActivity {
         recyclerView.setAdapter(adapter);
         adapter.startListening();
     }
+
+    private void checkout() {
+        txtTotalPrice.setText("Total: " + totalPrice + "$");
+
+    }
+
 
 }
